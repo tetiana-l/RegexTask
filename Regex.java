16b2462971f98181aeaf59c2b7f4164a2bc4727f
Tetiana <88380964+tetiana-l@users.noreply.github.com>
2021-09-03 17:11:36 +0300

    Add files via upload

---
+++ Regex.java
@@ -0,0 +1,40 @@
+package Task06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public static void main(String[] args) {
+        String str = "2020/12/23 05:30";
+        checkData(str);
+        str = "2020/13/77 99:99";
+        checkData(str);
+        str = "abc@mail.com";
+        returnDomain(str);
+        str = "zzz@ccc.v";
+        returnDomain(str);
+
+    }
+
+    public static void checkData(String str) {
+
+        String regex = "[1-9][0-9]{3}/(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[0-2])\\s([01][0-9]|2[0-3]):([0-5][0-9])";
+
+        boolean result = str.matches(regex);
+        System.out.println(result);
+    }
+
+    public static void returnDomain(String str) {
+
+        String regex = "[^@ \\t\\r\\n]+@([^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            String group = matcher.group(1);
+            System.out.println(group);
+        }
+    }
+}
